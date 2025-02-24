c83e1343b527d1d957872829792ec8f28f76ec9c
Софія Щербатюк <sfchbtik@gmail.com>
2025-02-24 18:06:53 +0200

    Задаю кольори та створюю об'єкти лабіринт/гра/стан гри

--- main.py
+++ main.py
@@ -1,24 +1,57 @@
 import pygame
+import sys
+from game_state import GameState
+from maze import Maze
+from game.game_manager import GameManager
 
-# Инициализация Pygame
+# Ініціалізація Pygame
 pygame.init()
 
 # Налаштування вікна
 WIDTH, HEIGHT = 600, 600
+CELL_SIZE = 40
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pac-Man")
 clock = pygame.time.Clock()
 
+# Кольори
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+# Створення гри
+maze = Maze()
+game = GameManager(maze)
+game_state = GameState()
+
+#намалювати стіни
+
+
+#намалювати самого пакмена
+
+
+
+#намалювати привида
+
+
+
+# Функція для відображення рахунку та кількості життів
+
+
+
+
 # Основний цикл гри
 running = True
 while running:
-    screen.fill((0, 0, 0))  # Очищення екрану
+    screen.fill(BLACK)  # Очищення екрану
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    pygame.display.flip()  # Оновлення екрану
-    clock.tick(10)  # Обмеження FPS
-
+    pygame.display.flip()  # Оновлення
+    clock.tick(10)  # Обмеження FPS до 10
 pygame.quit()
+sys.exit()
