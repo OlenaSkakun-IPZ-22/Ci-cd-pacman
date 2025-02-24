14bc90a6d148c2b5babba6a245669d1c1516cd4a
OlenaSkakun-IPZ-22 <olena_skakunn@knu.ua>
2025-02-24 16:55:16 +0200

    Створила клас point.py

---
+++ point.py
@@ -0,0 +1,10 @@
+class Point:
+    def __init__(self, x: int, y: int, is_power_up: bool = False):
+        """Точка, яку може збирати Pac-Man"""
+        self.x = x
+        self.y = y
+        self.is_power_up = is_power_up  # Якщо точка є енергетичною
+
+    def consume(self):
+        """Pac-Man з'їдає точку"""
+        pass
